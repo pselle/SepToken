6410f1dbd9ff3551af3981519f78f69a9b038819
Pam Selle <pamela.selle@gmail.com>
2012-08-04 12:06:09 -0400

    Create activity, add Fragments

---
+++ src/com/example/septoken/ListFragment.java
@@ -0,0 +1,11 @@
+package com.example.septoken;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public class ListFragment extends Fragment {
+
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+	}
+}
---
+++ src/com/example/septoken/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.septoken;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
